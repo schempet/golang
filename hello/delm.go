1cd213a361f1a297f8b66a645e42861be63b45dd
Shijith Chempeth <chempeth@gmail.com>
2016-12-06 17:27:12 -0800

    delte now ..

---
+++ hello/delm.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("Hello, world.\n")
+	fmt.Printf("2. Hello, world.\n")
+	fmt.Printf("2. Hello, world.\n")
+	fmt.Printf("2. Hello, world.\n")
+	fmt.Printf("2. Hello, Friday.\n")
+	fmt.Printf("2. Hello, world.\n")
+}
